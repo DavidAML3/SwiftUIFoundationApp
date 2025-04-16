3bec94680e34ecce08bc01981318de9c98de251a
David Andres Mejia Lopez <daml@Davids-MacBook-Pro.local>
2025-04-16 11:06:03 -0500

    Initial commit: SwiftUI Foundation App project setup

---
+++ SwiftUIFoundationApp/SwiftUIFoundationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFoundationAppApp.swift
+//  SwiftUIFoundationApp
+//
+//  Created by David Andres Mejia Lopez on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFoundationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIFoundationAppTests/SwiftUIFoundationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFoundationAppTests.swift
+//  SwiftUIFoundationAppTests
+//
+//  Created by David Andres Mejia Lopez on 16/04/25.
+//
+
+import Testing
+@testable import SwiftUIFoundationApp
+
+struct SwiftUIFoundationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
